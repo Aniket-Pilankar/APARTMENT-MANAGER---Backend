b0e6fcd98df496efd4b551b53339705332f4af5c
Aniket-Pilankar <aniketpilankar1998@gmail.com>
2022-04-22 03:08:45 +0530

    patch added

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const cors = require('cors')
 const app = express();
 const connect = require('./server/src/configs/db')
 app.use(express.json())
-app.use(cors())
-// const corsOptions ={
-//     origin:'http://localhost:3000', 
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
+// app.use(cors())
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 
 const {register,login} = require('./server/src/controllers/auth.controller')
