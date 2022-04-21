b3b9dde315b9439943e3536b66f9b2ee4476af34
Aniket-Pilankar <aniketpilankar1998@gmail.com>
2022-04-22 01:08:54 +0530

    cors added

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const cors = require('cors')
 const app = express();
 const connect = require('./server/src/configs/db')
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 
 const {register,login} = require('./server/src/controllers/auth.controller')
