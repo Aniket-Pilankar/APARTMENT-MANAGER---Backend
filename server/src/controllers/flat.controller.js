6e98121c27316c99905a1b37aed0ba89a5ca8dec
Aniket-Pilankar <aniketpilankar1998@gmail.com>
2022-04-22 19:00:52 +0530

    sorting route done

--- server/src/controllers/flat.controller.js
+++ server/src/controllers/flat.controller.js
@@ -71,8 +71,12 @@ router.get('/sortBy/:val',async(req,res) => {
         // // return res.send(flat)
         console.log(req.params.val)
         let value = req.params.val
-        const flat = await Flat.find().sort({"BlockNo":value}).lean().exec()
-        return res.send(flat)
+        const page = req.query.page || 1;
+        const size = req.query.size || 5;
+        // const flat = await Flat.find().sort({"BlockNo":value}).lean().exec()
+        const flat = await Flat.find().sort({"BlockNo":value}).skip((page - 1) * size).limit(size).populate({path:'resident_id'}).lean().exec()
+        const totalPage = Math.ceil(await Flat.find().countDocuments() / size) 
+        return res.send({flat,totalPage})
 
     } catch (error) {
         return res.send('error in flat controller /sortBy:',error)
